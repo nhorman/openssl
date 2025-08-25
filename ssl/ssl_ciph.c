6f6e8852721303ea9180dca3f53fddbb985ca12a
Neil Horman <nhorman@openssl.org>
2025-08-25 18:47:36 -0400

    remove no-longer needed call to tls_release_engine
    With the removal of ENGINES this call is no longer needed

--- ssl/ssl_ciph.c
+++ ssl/ssl_ciph.c
@@ -293,7 +293,6 @@ static int get_optional_pkey_id(const char *pkey_name)
                                     ameth) <= 0)
             pkey_id = 0;
     }
-    tls_engine_finish(tmpeng);
     return pkey_id;
 }
 #else
