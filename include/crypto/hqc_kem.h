ee34f044942e87b267f0b4b9b12d5ec6210ce7f5
Neil Horman <nhorman@openssl.org>
2025-10-14 14:12:10 -0400

    Add HQC keymgmt
    Heavily borrowed/adapted from the public domain reference implementation
    at:
    https://gitlab.com/pqc-hqc/hqc/
    
    This commit adds HQC key management functionality.  Currently key
    generation and import is supported, with export as an outstanding todo.
    
    evp tests are added for HQC-1/3/5 using the kats tests from the
    reference implementation

--- include/crypto/hqc_kem.h
+++ include/crypto/hqc_kem.h
@@ -11,12 +11,34 @@
 #define OPENSSL_HEADER_HQC_KEM_H
 #pragma once
 
+typedef enum {
+    EVP_PKEY_HQC_KEM_128 = 0,
+    EVP_PKEY_HQC_KEM_192 = 1,
+    EVP_PKEY_HQC_KEM_256 = 2,
+    EVP_PKEY_HQC_KEM_MAX
+} hqc_key_type;
+
+typedef struct hqc_variant_info_st {
+    hqc_key_type type;
+    size_t ek_size;
+    size_t dk_size;
+    size_t seed_len;
+    size_t security_bytes;
+    uint32_t security_category;
+    uint32_t secbits;
+    uint32_t n;
+    uint32_t n_mu;
+    uint16_t omega;
+    uint16_t omega_r;
+    uint32_t rej_threshold;
+} HQC_VARIANT_INFO;
+
 /* Known as HQC_KEY via crypto/types.h */
 typedef struct ossl_hqc_kem_key_st {
-    uint8_t *ek_pke; /* encryption key */
-    uint8_t *dk_pke; /* decryption key */
-    size_t ek_size; /* encryption key length */
-    size_t dk_size; /* decryption key length */
+    const HQC_VARIANT_INFO *info; /* key size info */
+    uint8_t *ek; /* encryption key */
+    uint8_t *dk; /* decryption key */
+    int selection; /* Presence status of key parts */
 } HQC_KEY;
 
 #endif /* OPENSSL_HEADER_HQC_KEM_H */
