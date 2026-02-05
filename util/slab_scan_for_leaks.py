0e77f58d11ccc7248256968633786435dd1702b3
Neil Horman <nhorman@openssl.org>
2026-02-05 14:21:18 -0500

    adding mmap reporting to slab scaning python script

--- util/slab_scan_for_leaks.py
+++ util/slab_scan_for_leaks.py
@@ -17,13 +17,14 @@ def scan_for_leaks(objtype, csvarray):
     for index, entry in enumerate(csvarray):
         if entry["info"][0] == objtype and entry["info"][2] == "event:allocate":
             indexlist.append(index)
-    print(f"Scanning for leaks in {objtype}, {len(indexlist)} instances")
+    print(f"Scanning for leaks in {objtype}, {len(indexlist)} allocations")
     for idx in indexlist:
         find_matching_free(idx, csvarray)
 
 
 def main(argv):
     allocatorarray = []
+    mmaparray = []
     slabarray = []
     objarray = []
     nonslabobjarray = []
@@ -33,6 +34,8 @@ def main(argv):
         csvreader = csv.reader(csvfile, delimiter='|')
         for row in csvreader:
             entry = { "index" : idx, "info" : row } 
+            if row[0] == "type:mmap":
+                mmaparray.append(entry)
             if row[0] == "type:allocator":
                 allocatorarray.append(entry)
             elif row[0] == "type:slab":
@@ -43,6 +46,7 @@ def main(argv):
                 nonslabobjarray.append(entry)
             idx = idx + 1
         scan_for_leaks("type:allocator", allocatorarray)
+        scan_for_leaks("type:mmap", mmaparray)
         scan_for_leaks("type:slab", slabarray)
         scan_for_leaks("type:obj", objarray)
         scan_for_leaks("type:nonslab-obj", nonslabobjarray)
