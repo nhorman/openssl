92d646412f9a0c3beb596acdc89011efa8f5fd1c
Neil Horman <nhorman@openssl.org>
2026-02-27 10:44:55 -0500

    Remove hashfunc debug code

--- crypto/hashtable/hashfunc.c
+++ crypto/hashtable/hashfunc.c
@@ -7,14 +7,13 @@
  * https://www.openssl.org/source/license.html
  *
  */
-
 #include "internal/hashfunc.h"
 
 #define FNV1A_PRIME 0x00000100000001B3ULL
 ossl_unused uint64_t ossl_fnv1a_hash(uint8_t *key, size_t len)
 {
     uint64_t hash = 0xcbf29ce484222325ULL;
-    size_t i = len;
+    ssize_t i = (ssize_t) len;
 
 #ifndef DO_LOOP_UNROLL
     for (i = 0; i < len; i++) {
@@ -25,46 +24,76 @@ ossl_unused uint64_t ossl_fnv1a_hash(uint8_t *key, size_t len)
 #else
     uint8_t *keytmp = key;
     do {
-        switch (i % 8) {
+        switch (i % 16) {
         case 0:
-            if (len == 0)
-                break;
-            hash ^= keytmp[0];
+            hash ^= keytmp[15];
             hash *= FNV1A_PRIME;
             /* FALLTHROUGH */
-        case 7:
+        case 15:
+            hash ^= keytmp[14];
+            hash *= FNV1A_PRIME;
+            /* FALLTHROUGH */
+        case 14:
+            hash ^= keytmp[13];
+            hash *= FNV1A_PRIME;
+            /* FALLTHROUGH */
+        case 13:
+            hash ^= keytmp[12];
+            hash *= FNV1A_PRIME;
+            /* FALLTHROUGH */
+        case 12:
+            hash ^= keytmp[11];
+            hash *= FNV1A_PRIME;
+            /* FALLTHROUGH */
+        case 11:
+            hash ^= keytmp[10];
+            hash *= FNV1A_PRIME;
+            /* FALLTHROUGH */
+        case 10:
+            hash ^= keytmp[9];
+            hash *= FNV1A_PRIME;
+            /* FALLTHROUGH */
+        case 9:
+            hash ^= keytmp[8];
+            hash *= FNV1A_PRIME;
+            /* FALLTHROUGH */
+        case 8:
             hash ^= keytmp[7];
             hash *= FNV1A_PRIME;
             /* FALLTHROUGH */
-        case 6:
+        case 7:
             hash ^= keytmp[6];
             hash *= FNV1A_PRIME;
             /* FALLTHROUGH */
-        case 5:
+        case 6:
             hash ^= keytmp[5];
             hash *= FNV1A_PRIME;
             /* FALLTHROUGH */
-        case 4:
+        case 5:
             hash ^= keytmp[4];
             hash *= FNV1A_PRIME;
             /* FALLTHROUGH */
-        case 3:
+        case 4:
             hash ^= keytmp[3];
             hash *= FNV1A_PRIME;
             /* FALLTHROUGH */
-        case 2:
+        case 3:
             hash ^= keytmp[2];
             hash *= FNV1A_PRIME;
             /* FALLTHROUGH */
-        case 1:
+        case 2:
             hash ^= keytmp[1];
+            hash *= FNV1A_PRIME;
+            /* FALLTHROUGH */
+        case 1:
+            hash ^= keytmp[0];
             hash *= FNV1A_PRIME;
             /* FALLTHROUGH */
         default:
             break;
         }
-        keytmp += 8;
-        i -= 8;
+        keytmp += 16;
+        i -= 16;
     } while (i > 0);
 #endif 
     return hash;
