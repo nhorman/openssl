a39a41d792c9ee7a5fcf403195c51aa1bc283eff
Neil Horman <nhorman@openssl.org>
2026-09-21 15:02:23 -0400

    fixup! Add a test to check all algorithms for the presence of a fips indicator

--- test/fips_indicator_test.c
+++ test/fips_indicator_test.c
@@ -16,30 +16,143 @@
 
 static OSSL_LIB_CTX *libctx = NULL;
 
+/*
+ * Struct to store method specific functions for testing an indicator
+ * of a given EVP type
+ */
+struct evp_method_fns {
+    void *(*alloc_ctx)(void);
+    void (*free_ctx)(void *ctx);
+    const char *(*get_name)(void *alg);
+    void *(*alg_fetch)(OSSL_LIB_CTX *libctx, const char *name, const char *propq);
+    void (*alg_free)(void *alg);
+    int (*op_init)(void *ctx, void *alg);
+    int (*get_ctx_param)(void *ctx, OSSL_PARAM params[]);
+};
+
+/*
+ * Method functions for EVP_CIPHER
+ */
+static void *alloc_cipher_ctx(void)
+{
+    return EVP_CIPHER_CTX_new();
+}
+
+static void free_cipher_ctx(void *ctx)
+{
+    EVP_CIPHER_CTX_free((EVP_CIPHER_CTX *)ctx);
+}
+
+static const char *cipher_get_name(void *alg)
+{
+    return EVP_CIPHER_get0_name((EVP_CIPHER *)alg);
+}
+
+static void *cipher_fetch(OSSL_LIB_CTX *ctx, const char *name, const char *propq)
+{
+    return EVP_CIPHER_fetch(ctx, name, propq);
+}
+
+static void cipher_free(void *alg)
+{
+    EVP_CIPHER_free((EVP_CIPHER *)alg);
+}
+
+static int evp_cipher_init(void *ctx, void *alg)
+{
+    return EVP_DecryptInit_ex((EVP_CIPHER_CTX *)ctx, (EVP_CIPHER *)alg, NULL, NULL, NULL);
+}
+
+static int cipher_ctx_get_param(void *ctx, OSSL_PARAM params[])
+{
+    return EVP_CIPHER_CTX_get_params((EVP_CIPHER_CTX *)ctx, params);
+}
+
+static struct evp_method_fns cipher_methods = {
+    alloc_cipher_ctx,
+    free_cipher_ctx,
+    cipher_get_name,
+    cipher_fetch,
+    cipher_free,
+    evp_cipher_init,
+    cipher_ctx_get_param
+};
+
+/*
+ * Method functions for EVP_MD
+ */
+static void *alloc_md_ctx(void)
+{
+    return EVP_MD_CTX_new();
+}
+
+static void free_md_ctx(void *ctx)
+{
+    EVP_MD_CTX_free((EVP_MD_CTX *)ctx);
+}
+
+static const char *md_get_name(void *alg)
+{
+    return EVP_MD_get0_name((EVP_MD *)alg);
+}
+
+static void *md_fetch(OSSL_LIB_CTX *ctx, const char *name, const char *propq)
+{
+    return EVP_MD_fetch(ctx, name, propq);
+}
+
+static void md_free(void *alg)
+{
+    EVP_MD_free((EVP_MD *)alg);
+}
+
+static int evp_md_init(void *ctx, void *alg)
+{
+    return EVP_DigestInit((EVP_MD_CTX *)ctx, (EVP_MD *)alg);
+}
+
+static int md_ctx_get_param(void *ctx, OSSL_PARAM params[])
+{
+    return EVP_MD_CTX_get_params((EVP_MD_CTX *)ctx, params);
+}
+
+static struct evp_method_fns digest_methods = {
+    alloc_md_ctx,
+    free_md_ctx,
+    md_get_name,
+    md_fetch,
+    md_free,
+    evp_md_init,
+    md_ctx_get_param
+};
+
 struct indicator_check_data_st {
     OSSL_LIB_CTX *ctx;
+    struct evp_method_fns *fns;
+    void *algctx;
     int ret;
 };
 
-static int check_cipher_indicator_params(OSSL_LIB_CTX *ctx, EVP_CIPHER_CTX *cctx,
+static int check_indicator_params(struct indicator_check_data_st *ind_data, void *cctx,
     const char *name, const char *propq,
     int expect_approved)
 {
-    EVP_CIPHER *testciph = NULL;
+    OSSL_LIB_CTX *ctx = ind_data->ctx;
+    void *testevp = NULL;
     int approved = -1;
     OSSL_PARAM testp[2] = { OSSL_PARAM_int(OSSL_ALG_PARAM_FIPS_APPROVED_INDICATOR, &approved), OSSL_PARAM_END };
     int ret = 0;
 
     /*
-     * Allocate an instance of this cipher from the requested provider
+     * Allocate an instance of this evp from the requested provider
      */
     ERR_clear_error();
-    testciph = EVP_CIPHER_fetch(ctx, name, propq);
-    if (testciph == NULL) {
+    testevp = ind_data->fns->alg_fetch(ctx, name, propq);
+    if (testevp == NULL) {
         /*
          * A missing alg from a provider isn't a failure, skip that
          */
-        TEST_note("Cipher %s is not present in any provider with %s, skipping", name, propq);
+        TEST_note("Alg %s is not present in any provider with %s, skipping", name, propq);
         ret = 1;
         goto out;
     }
@@ -49,13 +162,13 @@ static int check_cipher_indicator_params(OSSL_LIB_CTX *ctx, EVP_CIPHER_CTX *cctx
      * Note: We're doing decryption here because every fips approved algorithm
      * gets an approved indicator that we can test for
      */
-    if (!TEST_int_eq(EVP_DecryptInit_ex(cctx, testciph, NULL, NULL, NULL), 1))
+    if (!TEST_int_eq(ind_data->fns->op_init(cctx, testevp), 1))
         goto out;
 
     /*
      * Get the indicator
      */
-    if (!EVP_CIPHER_CTX_get_params(cctx, testp)) {
+    if (!ind_data->fns->get_ctx_param(cctx, testp)) {
         TEST_error("Failed to get indicator parameter for %s property %s", name, propq);
         goto out;
     }
@@ -74,140 +187,57 @@ static int check_cipher_indicator_params(OSSL_LIB_CTX *ctx, EVP_CIPHER_CTX *cctx
 
     ret = 1;
 out:
-    EVP_CIPHER_free(testciph);
+    ind_data->fns->alg_free(testevp);
     return ret;
+}
+
+static void check_fips_indicator(void *alg, void *arg)
+{
+    struct indicator_check_data_st *ind_data = arg;
+    const char *name = ind_data->fns->get_name(alg);
+    void *cctx = NULL;
+    int ret = 0;
+
+    /*
+     * Don't do any more testing if we've already failed
+     */
+    if (ind_data->ret == 0)
+        return;
+
+    /*
+     * Allocate a new EVP CTX
+     */
+    cctx = ind_data->fns->alloc_ctx();
+    if (!TEST_ptr(cctx)) {
+        ind_data->ret = 0;
+        return;
+    }
+
+    if (!check_indicator_params(ind_data, cctx, name, "provider=default", 0))
+        goto out;
+
+    if (!check_indicator_params(ind_data, cctx, name, "fips=yes", 1)) {
+        ERR_print_errors_fp(stderr);
+        goto out;
+    }
+
+    ret = 1;
+out:
+    ind_data->fns->free_ctx(cctx);
+    ind_data->ret = ret;
+    return;
 }
 
 static void check_cipher_fips_indicator(EVP_CIPHER *cph, void *arg)
 {
-    struct indicator_check_data_st *ind_data = arg;
-    const char *name = EVP_CIPHER_get0_name(cph);
-    EVP_CIPHER_CTX *cctx = NULL;
-    int ret = 0;
-
-    /*
-     * Don't do any more testing if we've already failed
-     */
-    if (ind_data->ret == 0)
-        return;
-
-    /*
-     * Allocate a new CIPHER CTX
-     */
-    cctx = EVP_CIPHER_CTX_new();
-    if (!TEST_ptr(cctx)) {
-        ind_data->ret = 0;
-        return;
-    }
-
-    if (!check_cipher_indicator_params(ind_data->ctx, cctx, name, "provider=default", 0))
-        goto out;
-
-    if (!check_cipher_indicator_params(ind_data->ctx, cctx, name, "fips=yes", 1)) {
-        ERR_print_errors_fp(stderr);
-        goto out;
-    }
-
-    ret = 1;
-out:
-    EVP_CIPHER_CTX_free(cctx);
-    ind_data->ret = ret;
-    return;
-}
-
-static int check_digest_indicator_params(OSSL_LIB_CTX *ctx, EVP_MD_CTX *cctx,
-    const char *name, const char *propq,
-    int expect_approved)
-{
-    EVP_MD *testmd = NULL;
-    int approved = -1;
-    OSSL_PARAM testp[2] = { OSSL_PARAM_int(OSSL_ALG_PARAM_FIPS_APPROVED_INDICATOR, &approved), OSSL_PARAM_END };
-    int ret = 0;
-
-    /*
-     * Allocate an instance of this cipher from the requested provider
-     */
-    ERR_clear_error();
-    testmd = EVP_MD_fetch(ctx, name, propq);
-    if (testmd == NULL) {
-        /*
-         * A missing alg from a provider isn't a failure, skip that
-         */
-        TEST_note("Digest %s is not present in any provider with %s, skipping", name, propq);
-        ret = 1;
-        goto out;
-    }
-
-    /*
-     * Init the context for decryption
-     * Note: We're doing decryption here because every fips approved algorithm
-     * gets an approved indicator that we can test for
-     */
-    if (!TEST_int_eq(EVP_DigestInit(cctx, testmd), 1))
-        goto out;
-
-    /*
-     * Get the indicator
-     */
-    if (!EVP_MD_CTX_get_params(cctx, testp)) {
-        TEST_error("Failed to get indicator parameter for %s property %s", name, propq);
-        goto out;
-    }
-
-    if (!OSSL_PARAM_get_int(&testp[0], &approved)) {
-        TEST_error("Failed to extract integer param for %s property %s", name, propq);
-        goto out;
-    }
-
-    if (approved != expect_approved) {
-        TEST_error("Alg %s property %s got approved %d expected %d", name, propq, approved, expect_approved);
-        goto out;
-    }
-
-    TEST_note("Alg %s property %s is %s", name, propq, approved ? "approved" : "not approved");
-
-    ret = 1;
-out:
-    EVP_MD_free(testmd);
-    return ret;
+    check_fips_indicator((void *)cph, arg);
 }
 
 static void check_digest_fips_indicator(EVP_MD *md, void *arg)
 {
-    struct indicator_check_data_st *ind_data = arg;
-    const char *name = EVP_MD_get0_name(md);
-    EVP_MD_CTX *cctx = NULL;
-    int ret = 0;
-
-    /*
-     * Don't do any more testing if we've already failed
-     */
-    if (ind_data->ret == 0)
-        return;
-
-    /*
-     * Allocate a new CIPHER CTX
-     */
-    cctx = EVP_MD_CTX_new();
-    if (!TEST_ptr(cctx)) {
-        ind_data->ret = 0;
-        return;
-    }
-
-    if (!check_digest_indicator_params(ind_data->ctx, cctx, name, "provider=default", 0))
-        goto out;
-
-    if (!check_digest_indicator_params(ind_data->ctx, cctx, name, "fips=yes", 1)) {
-        ERR_print_errors_fp(stderr);
-        goto out;
-    }
-
-    ret = 1;
-out:
-    EVP_MD_CTX_free(cctx);
-    ind_data->ret = ret;
-    return;
+    check_fips_indicator((void *)md, arg);
 }
+
 static int test_evp_alg_fips_indicator_present(void)
 {
     int ret = 0;
@@ -215,9 +245,12 @@ static int test_evp_alg_fips_indicator_present(void)
 
     ind_data.ctx = libctx;
     ind_data.ret = 1;
+    ind_data.fns = &cipher_methods;
     EVP_CIPHER_do_all_provided(libctx, check_cipher_fips_indicator, &ind_data);
     if (ind_data.ret == 0)
         goto out;
+
+    ind_data.fns = &digest_methods;
     EVP_MD_do_all_provided(libctx, check_digest_fips_indicator, &ind_data);
 
 out:
